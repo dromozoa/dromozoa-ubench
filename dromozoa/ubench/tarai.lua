a70a6c9a794b93acec0671fb9b7abdd9bd82c491
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-02-22 19:55:28 +0900

    taraiを分離。

--- dromozoa/ubench/tarai.lua
+++ dromozoa/ubench/tarai.lua
@@ -15,21 +15,12 @@
 -- You should have received a copy of the GNU General Public License
 -- along with dromozoa-ubench.  If not, see <http://www.gnu.org/licenses/>.
 
-local ubench = require "dromozoa.ubench"
-local timeval = require "dromozoa.ubench.timeval"
-local tarai = require "dromozoa.ubench.tarai"
+local function tarai(x, y, z)
+  if x <= y then
+    return y
+  else
+    return tarai(tarai(x - 1, y, z), tarai(y - 1, z, x), tarai(z - 1, x, y))
+  end
+end
 
-assert(ubench.RUSAGE_SELF)
-assert(ubench.RUSAGE_CHILDREN)
-
-local tv1 = ubench.gettimeofday()
-local ru1 = ubench.getrusage(ubench.RUSAGE_SELF)
-
-tarai(8, 4, 0)
-
-local tv2 = ubench.gettimeofday()
-local ru2 = ubench.getrusage(ubench.RUSAGE_SELF)
-
-print("tv", timeval(tv2) - timeval(tv1))
-print("ut", timeval(ru2.ru_utime) - timeval(ru1.ru_utime))
-print("st", timeval(ru2.ru_stime) - timeval(ru1.ru_stime))
+return tarai
