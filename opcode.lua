562ff7e8b2fdfd189ac223a9e78eb0b72985b5ba
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-04-08 09:07:02 +0900

    命令コードの調査のためのコードを修正。

--- opcode.lua
+++ opcode.lua
@@ -1,111 +1,154 @@
-local math_random = math.random
-local math_randomseed = math.randomseed
-local table_concat = table.concat
-local table_unpack = table.unpack or unpack
+local t = {}
+local u
 
-math_randomseed(42)
-
-local phonetic_alphabet = {
-  "alfa", "bravo", "charlie", "delta", "echo",
-  "foxtrot", "golf", "hotel", "india", "juliett",
-  "kilo", "lima", "mike", "november", "oscar",
-  "papa", "quebec", "romeo", "sierra", "tango",
-  "uniform", "victor", "whiskey", "xray", "yankee",
-  "zulu",
-}
-
-local function make_integer_array(m, n)
-  local data = {}
-  for i = 1, m do
-    data[i] = math_random(n)
-  end
-  return data
+local function MOVE(a, b)
+  a = b
 end
 
-local function make_number_array(m)
-  local data = {}
-  for i = 1, m do
-    data[i] = math_random()
-  end
-  return data
+local function LOADK(a)
+  a = 42
 end
 
-local function make_string_array(m, n)
-  local data = {}
-  for i = 1, m do
-    local buffer = {}
-    for j = 1, n do
-      buffer[j] = phonetic_alphabet[math_random(26)]
-    end
-    data[i] = table_concat(buffer, "_")
-  end
-  return data
+local function LOADBOOL(a)
+  a = true
 end
 
-local function make_boolean_array(m)
-  local data = {}
-  for i = 1, m do
-    data[i] = math_random(2) == 1
-  end
-  return data
+local function LOADNIL()
+  local a
 end
 
-local function alfa   () return 1 end
-local function bravo  () return 2 end
-local function charlie() return 3 end
-local function delta  () return 4 end
-local function echo   () return 5 end
-local function foxtrot() return 6 end
-local function golf   () return 7 end
-local function hotel  () return 8 end
-
-local this = {
-  alfa    = alfa;
-  bravo   = bravo;
-  charlie = charlie;
-  delta   = delta;
-  echo    = echo;
-  foxtrot = foxtrot;
-  golf    = golf;
-  hotel   = hotel;
-}
-
-local opcode = {}
-
-function opcode.add()
-  local r01, r02, r03, r04, r05, r06, r07, r08, r09, r10, r11, r12, r13, r14, r15, r16
-  local d01, d02, d03, d04, d05, d06, d07, d08, d09, d10, d11, d12, d13, d14, d15, d16 = table_unpack(make_integer_array(16, 65536))
-  local n01, n02, n03, n04, n05, n06, n07, n08, n09, n10, n11, n12, n13, n14, n15, n16 = table_unpack(make_number_array(16))
-  local b01, b02, b03, b04, b05, b06, b07, b08, b09, b10, b11, b12, b13, b14, b15, b16 = table_unpack(make_boolean_array(16))
-  local this = this
-
---[[
-  r01 = d01 + d02
-  r02 = d03 + d04
-  r03 = d05 + d06
-  r04 = d07 + d08
-  r05 = d09 + d10
-  r06 = d11 + d12
-  r07 = d13 + d14
-  r08 = d15 + d16
-]]
-
-  r01 = this:alfa()
-  r02 = this:bravo()
-  r03 = this:charlie()
-  r04 = this:delta()
-  r05 = this:echo()
-  r06 = this:foxtrot()
-  r07 = this:golf()
-  r08 = this:hotel()
+local function GETUPVAL(a)
+  a = u
 end
 
-local function f() end
-local function g() end
+local function GETTABUP(a)
+  a = t[1]
+end
 
-print(f)
-print(g)
-print(f == f)
-print(f == g)
+local function GETTABLE(a, b)
+  a = b[1]
+end
 
-return opcode
+local function SETTABUP(a)
+  t[1] = a
+end
+
+local function SETUPVAL(a)
+  u = a
+end
+
+local function NEWTABLE()
+  local a = {}
+end
+
+local function SELF(a)
+  a:f()
+end
+
+local function ADD(a, b, c)
+  a = b + c
+end
+
+local function SUB(a, b, c)
+  a = b - c
+end
+
+local function MUL(a, b, c)
+  a = b * c
+end
+
+local function MOD(a, b, c)
+  a = b % c
+end
+
+local function POW(a, b, c)
+  a = b ^ c
+end
+
+local function DIV(a, b, c)
+  a = b / c
+end
+
+local function IDIV(a, b, c)
+  a = b // c
+end
+
+local function BAND(a, b, c)
+  a = b & c
+end
+
+local function BOR(a, b, c)
+  a = b | c
+end
+
+local function BXOR(a, b, c)
+  a = b ~ c
+end
+
+local function SHL(a, b, c)
+  a = b << c
+end
+
+local function SHR(a, b, c)
+  a = b >> c
+end
+
+local function UNM(a, b)
+  a = -b
+end
+
+local function BNOT(a, b)
+  a = ~b
+end
+
+local function NOT(a, b)
+  a = not b
+end
+
+local function LEN(a, b)
+  a = #b
+end
+
+local function CONCAT(a, b, c)
+  a = b .. c
+end
+
+local function EQ(a, b)
+  if a == b then end
+end
+
+local function LT(a, b)
+  if a < b then end
+end
+
+local function LE(a, b)
+  if a <= b then end
+end
+
+local function TEST(a)
+  if a then end
+end
+
+local function TESTSET(a, b, c)
+  a = b and c
+end
+
+local function FORLOOP(a, b)
+  for i = a, b do end
+end
+
+local function TFORLOOP(a)
+  for i in a do end
+end
+
+local function SETLIST(a)
+  a = { 42 }
+end
+
+local function CLOSURE(a)
+  local function f() end
+end
+
+local function VARARG(a, ...)
+  a = ...
+end
---
+++ opcode_x.lua
@@ -0,0 +1,13 @@
+local function f()
+  local r = 0
+  r = r + 10
+  r = r + 11
+  r = r + 12
+  r = r + 13
+  r = r + 14
+  r = r + 15
+  r = r + 16
+  r = r + 17
+  r = r + 18
+  r = r + 19
+end
