707f7327bcda195761ce3913ab3e0f3a79d14001
Tomoyuki Fujimori <moyu@dromozoa.com>
2015-04-23 23:55:05 +0900

    実行方式を変更。

--- dromozoa/ubench/run.lua
+++ dromozoa/ubench/run.lua
@@ -15,9 +15,74 @@
 -- You should have received a copy of the GNU General Public License
 -- along with dromozoa-ubench.  If not, see <http://www.gnu.org/licenses/>.
 
-local estimate_n = require "dromozoa.ubench.estimate_n"
-local run2 = require "dromozoa.ubench.run2"
+local gettimeofday = require "dromozoa.ubench.gettimeofday"
+local stdev = require "dromozoa.ubench.stdev"
 
-return function (m, u, fn)
-  return run2(m, estimate_n(u, fn), fn)
+local function run(n, fn)
+  collectgarbage()
+  collectgarbage()
+
+  local tv1 = gettimeofday()
+  for i = 1, n do
+    fn()
+  end
+  local tv2 = gettimeofday()
+
+  local s = tv2.tv_sec - tv1.tv_sec
+  local u = tv2.tv_usec - tv1.tv_usec
+  if u < 0 then
+    s = s - 1
+    u = u + 1000000
+  end
+  return s * 1000000 + u
+end
+
+local function estimate(u, fn)
+  local a = u * 0.99
+  local b = u * 1.01
+  local n = 1
+  while true do
+    local t = run(n, fn)
+    if a <= t and t < b then
+      return n
+    end
+    if t < 1 then t = 1 end
+    local m = n * u / t
+    m = m - m % 1
+    if m < 1 then m = 1 end
+    if n == m then
+      return n
+    end
+    n = m
+  end
+end
+
+return function (F, m, u)
+  local N = {}
+  local T = {}
+
+  for i = 1, #F do
+    N[i] = estimate(u, F[i])
+    T[i] = {}
+  end
+
+  for j = 1, m do
+    for i = 1, #F do
+      local n = N[i]
+      T[i][j] = run(n, F[i]) / n
+    end
+  end
+
+  local a = m / 8
+  local b = a * 3
+  a = a - a % 1
+  b = b - b % 1
+
+  local R = {}
+  for i = 1, #T do
+    table.sort(T[i])
+    R[i] = { stdev.s(T[i], a, b) }
+  end
+
+  return R
 end
